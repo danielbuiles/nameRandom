691cecb9a19fba22e5e8b0e31b064433941d8ca9
Daniel Builes <danielbgar@gmail.com>
2021-12-08 11:38:38 -0500

    first application with svelte

---
+++ src/App.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+	async function getRandomName(){
+		return await fetch("https://random-data-api.com/api/name/random_name")
+			.then(response => response.json())
+			.then(data => data.name);
+	}
+
+	let promise = getRandomName();
+	function newName(){
+		promise=getRandomName();
+	}
+</script>
+
+<button on:click="{newName}">
+	get Random Name
+</button>
+
+{#await promise}
+	<p>.......</p>
+{:then nombre} 
+	<p>nombre:{nombre}</p>
+{/await}
